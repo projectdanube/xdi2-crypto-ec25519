5d162b5936b6ca69d8e9dfe49810619ad1e01400
peacekeeper <markus@projectdanube.org>
2015-10-13 17:58:02 +0200

    dont need these fields

--- src/main/java/xdi2/core/security/ec25519/signature/validate/EC25519CloudNumberPublicKeySignatureValidator.java
+++ src/main/java/xdi2/core/security/ec25519/signature/validate/EC25519CloudNumberPublicKeySignatureValidator.java
@@ -1,8 +1,6 @@
 package xdi2.core.security.ec25519.signature.validate;
 
 import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.Map;
 
 import xdi2.core.security.ec25519.util.EC25519CloudNumberUtil;
 import xdi2.core.syntax.CloudNumber;
@@ -10,25 +8,9 @@ import xdi2.core.syntax.XDIAddress;
 
 /**
  * This is an ECC25519PublicKeySignatureValidater that validate an XDI ECC25519Signature by
- * obtaining public keys from a statically configured list.
+ * extracting a public key from an XDI address (cryptographic cloud number).
  */
 public class EC25519CloudNumberPublicKeySignatureValidator extends EC25519PublicKeySignatureValidator {
-
-	private Map<XDIAddress, byte[]> publicKeys;
-
-	public EC25519CloudNumberPublicKeySignatureValidator(Map<XDIAddress, byte[]> publicKeys) {
-
-		super();
-
-		this.publicKeys = publicKeys;
-	}
-
-	public EC25519CloudNumberPublicKeySignatureValidator(byte[] publicKey) {
-
-		super();
-
-		this.publicKeys = Collections.singletonMap(null, publicKey);
-	}
 
 	public EC25519CloudNumberPublicKeySignatureValidator() {
 
@@ -49,25 +31,5 @@ public class EC25519CloudNumberPublicKeySignatureValidator extends EC25519Public
 		// done
 
 		return publicKey;
-	}
-
-	public Map<XDIAddress, byte[]> getPublicKeys() {
-
-		return this.publicKeys;
-	}
-
-	public void setPublicKeys(Map<XDIAddress, byte[]> publicKeys) {
-
-		this.publicKeys = publicKeys;
-	}
-
-	public byte[] getPublicKey() {
-
-		return this.publicKeys.get(null);
-	}
-
-	public void setPublicKey(byte[] publicKey) {
-
-		this.publicKeys.put(null, publicKey);
 	}
 }
